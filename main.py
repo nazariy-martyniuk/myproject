9b8a64e71b74b87e46e019025d4183f158952f1d
unknown <nazya19.martvad@gmil.com>
2022-12-09 09:26:50 +0200

    completed

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Как я устал, я хочу спать!')
+with open('text.txt', 'r') as file:
+    file_data = file.read()
+
+file_data = file_data.replace('word1', 'word2')
+
+with open('text.txt', 'w') as file:
+    file.write(file_data)
